c4eeee78a76698e0c3527afa672b110e28a6c80d
mar2544 <hiuelang1@gmail.com>
2025-03-08 12:31:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Terima kasih telah menghubungi kami! Kami akan segera merespons.');
+});
